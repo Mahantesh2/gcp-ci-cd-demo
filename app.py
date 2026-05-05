d037d53c61e5709cce57dc5f5df8357ec62e8cba
Mahantesh M B <mahanteshmb21@gmail.com>
2026-05-05 22:45:27 +0530

    app.py file tjust to check if there is an update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("Hello mathu its great the Cloud Build CI/CD Pipeline is working fine")
 
-print
+print("This is a sample Python application to demonstrate the Cloud Build CI/CD Pipeline.") 
 
